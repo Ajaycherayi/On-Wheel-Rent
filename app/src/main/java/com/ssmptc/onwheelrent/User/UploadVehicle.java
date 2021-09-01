0dc809e2571bc5be6c3ef1ca9cebf0c9d78a24d1
AjayCherayi <cajaycs@gmail.com>
2021-09-01 22:01:45 +0530

    Ui Design setup

--- app/src/main/java/com/ssmptc/onwheelrent/User/UploadVehicle.java
+++ app/src/main/java/com/ssmptc/onwheelrent/User/UploadVehicle.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.ssmptc.onwheelrent.R;
 
-public class InsertVehicle extends AppCompatActivity {
+public class UploadVehicle extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_insert_vehicle);
+        setContentView(R.layout.activity_upload_vehicle);
     }
 }
